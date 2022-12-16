ff85b1a268b78b5c566353d62181174aad197e3c
Viktoria <zaharova181114@gmail.com>
2022-12-16 20:29:00 +0200

    edit1

--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
 $('[name="phone"]').mask('+7 (999) 999-99-99');
 
+// scroll top
 $("body").on("click", ".btn-scroll-top", function (e) {
 	e.preventDefault();
 	$("html, body").animate({
@@ -7,6 +8,7 @@ $("body").on("click", ".btn-scroll-top", function (e) {
 	}, "slow");
 });
 
+// scroll link anchor
 $('.go_to').click(function (e) {
 	e.preventDefault();
 	var scroll_el = $(this).attr('href');
@@ -14,6 +16,19 @@ $('.go_to').click(function (e) {
 		$('html, body').animate({
 			scrollTop: $(scroll_el).offset().top
 		}, 500);
+
+		$('.mobile-menu').fadeOut();
 	}
 	return false;
+});
+
+// mobile menu
+$('.btn-burger').on('click', function (e) {
+	e.preventDefault();
+	$('.mobile-menu').fadeIn();
+});
+
+$('.mobile-menu__close').on('click', function (e) {
+	e.preventDefault();
+	$('.mobile-menu').fadeOut();
 });
